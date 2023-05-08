76eff25b993d7f23ac5e9d3ba46469d75479d458
冯曦 <114207893+WinkFxJs@users.noreply.github.com>
2023-05-08 16:21:14 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wx26ad86a9b3a162a0";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "5e034913a62d4aaf232732afc4c1a6cf";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "68saFCoxtgQS1N_aolPW-Slg_EdiCAmJi31EEaucU6M";
 
     /**
      * 初始化
