3d79febc4062d573943d7f1057381c885590bd04
冯曦 <114207893+WinkFxJs@users.noreply.github.com>
2023-05-08 16:24:11 +0800

    Update Application.java

--- src/main/java/cn/ofpp/Application.java
+++ src/main/java/cn/ofpp/Application.java
@@ -32,10 +32,6 @@ public class Application {
                 "安徽省", "宣城市", "2004-04-11", "2023-05-02", "ol0Wd54lFUsUzWCvEYh_DfKKZUYA");
         Wx.sendTemplateMessage(MessageFactory.resolveMessage(girlFriend));
 
-        // new 一个 男友 也可单独针对一个friend设置模板ID 以达到不同人不同消息
-        BoyFriend boyFriend = new BoyFriend("某男友",
-                "江苏省", "南京市", "1999-08-08", "2011-04-16", "oQFk-5qtXv2uGNCu9oiCiV85KWD8", "5t7-Ksy8_rw-QmUkxf8J7Pe-QLQ2rBc7RWJi_pSmeh4");
-        Wx.sendTemplateMessage(MessageFactory.resolveMessage(boyFriend));
     }
 
 }
